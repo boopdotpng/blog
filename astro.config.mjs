7cab3faef7e2ba8f7995e1b6bb7daed296d9bbf9
anuraag <anuraag.warudkar@gmail.com>
2025-04-17 00:52:37 -0400

    more seo improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://anuraagw.me',
   trailingSlash: 'never',
+  integrations: [sitemap()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
