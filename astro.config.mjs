bf6af43aa609e769019d7fe1c7e2497fc912df5d
anuraag <anuraag.warudkar@gmail.com>
2025-04-17 00:04:45 -0400

    add deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import remarkMath from 'remark-math';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://anuraagw.me',
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
